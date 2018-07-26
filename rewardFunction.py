e00727523184eacb0e6d4ad39545cfa3ae46a6f1
Zac Moran <zac.moran@icloud.com>
2018-07-26 16:42:23 +1000

    Add files via upload

--- rewardFunction.py
+++ rewardFunction.py
@@ -6,6 +6,7 @@ Created on Thu Jul 26 13:00:10 2018
 """
 
 import pandas as pd
+import random as r
 
 data = pd.read_csv('afl.csv', encoding = "ISO-8859-1",header=1)
 
@@ -13,35 +14,59 @@ homeWin = []
 
 scores = data.iloc[:,5:7].values
 
+odds = data.iloc[:,12:14].values
+
 for i in range(len(scores)):
     homeWin.append(scores[i][0] > scores[i][1])
+
+flatOdds = []
+holdOdds = []
+
+realResults = []
+holdresults = []
+action = []
+actions = []
+
+
+for j in range(207):
+    holdOdds = []
+    holdresults =  []
+    action = []
+    for i in range(9):
+        holdOdds.append(odds[i][0])
+        holdOdds.append(odds[i][1])
+        holdresults.append(int(homeWin[i]))
+        action.append([r.randint(0,2),r.randint(0,2),r.randint(0,2),r.randint(0,100)])
+    flatOdds.append(holdOdds)
+    realResults.append(holdresults)
+    actions.append(action)
+    
   
     
 # 0 = away bet
 # 1 = home bet
 # 2 = no bet
+    
+# 207 rounds of 9
+    
+
 
     
-action1 = [[1,2,0,10],[2,2,2,0],[2,1,2,5]]
-
-exampleRow = [1.3,2.5,1.1,2.2,2.1,3.4]
-
-gameResults = [1,0,0]
 
 def reward(action,row,results):
     reward = 1
     for multi in action:
         success = True
-        if multi[len(action)]==0:
+        if multi[3]==0:
             continue
-        for i in range(len(action)):
+        for i in range(3):
             if multi[i]==2:
                 continue
             if multi[i]!=results[i]:
                 success = False
         if success:
             for j in range(len(multi)):
-                if (j == len(action) and multi[j]) != 0:
+                if j == len(action) and multi[j] != 0:
                     reward *= multi[j]
                 elif multi[j] == 0:
                     reward *= row[j*2+1]
@@ -52,5 +77,37 @@ def reward(action,row,results):
         return 0               
     return reward
 
-print(reward(action1,exampleRow,gameResults))
+def investment(action):
+    inv = 0
+    for multi in action:
+        inv += multi[3]
+    return inv
+
+def difference(inv,ret):
+    return ret-inv
+
+print(reward(actions[0],flatOdds[0],realResults[0]))
+
+print(investment(actions[0]))
+
+print("")
+
+print(difference(investment(actions[0]),reward(actions[0],flatOdds[0],realResults[0])))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                 
